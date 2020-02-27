149e50badb39553e25cfb90b7cceefe32e2ad508
Ezequiel Portela <eportelab@gmail.com>
2020-02-27 16:23:16 -0300

    update mysql repository

--- src/lib/mysqlRepository.js
+++ src/lib/mysqlRepository.js
@@ -1,15 +1,15 @@
 const {MysqlService} = require('../../index');
 
-const createMysqlClient = (options) => new MysqlService(options);
+const createClient = (options) => new MysqlService(options);
 
-const executeMysqlProcedure = async (instance, procName, params) =>
+const executeProcedure = async (instance, procName, params) =>
   await instance.executeProcedure(procName, params);
 
-const executeMysqlQuery = async (instance, query, params) =>
+const executeQuery = async (instance, query, params) =>
   await instance.executeProcedure(query, params);
 
 module.exports = {
-  createMysqlClient,
-  executeMysqlProcedure,
-  executeMysqlQuery,
+  createClient,
+  executeProcedure,
+  executeQuery,
 };
